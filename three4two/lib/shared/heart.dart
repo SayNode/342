bd00d4dbf15dbda779488058d4291587f73b9fe4
Werner Liechti <werner@saynode.ch>
2021-12-31 17:25:35 +0100

    Multipage

---
+++ three4two/lib/shared/heart.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Heart extends StatefulWidget {
+  @override
+  _HeartState createState() => _HeartState();
+}
+
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
+        .animate(_controller);
+
+    _controller.addListener(() {
+      print(_controller.value);
+      print(_colorAnimation.value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: _controller,
+        builder: (BuildContext context, _) {
+          return IconButton(
+            icon: Icon(Icons.favorite, color: _colorAnimation.value, size: 30),
+            onPressed: () {
+              _controller.forward();
+            },
+          );
+        });
+  }
+}
