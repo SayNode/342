f9870ff2e996976206eadbd539143de4322eb4ff
RsTs23 <43611813+RsTs23@users.noreply.github.com>
2022-02-04 17:03:21 +0100

    Implemented Tree planting

--- three4two/lib/widget/fetchTreeOffers.dart
+++ three4two/lib/widget/fetchTreeOffers.dart
@@ -9,6 +9,7 @@ import 'package:three4two/Utils/store.dart';
 import 'package:three4two/api/purchase_api.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
+import "package:hex/hex.dart";
 
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -57,8 +58,10 @@ Future fetchTreeOffers(BuildContext context) async {
 }
 
 Future purchase(context) async {
+  await plantTree(globals.newTreeName);
+  var baum = await getLatestTree();
   globals.recentTx =
-      await sendToScript(globals.message, globals.name1, globals.name2);
+      await sendToScript(globals.message, globals.name1, globals.name2, baum);
   writeJson(globals.txId.toString(), globals.recentTx);
   globals.txId++;
   Navigator.of(context).pushAndRemoveUntil(
@@ -66,12 +69,11 @@ Future purchase(context) async {
       (Route<dynamic> route) => false);
 }
 
-Future<String> sendToScript(nachricht, name1, name2) async {
+Future<String> plantTree(treename) async {
   await Future.delayed(Duration(seconds: 2));
   try {
-    Map form = {'nachricht': nachricht, 'name1': name1, 'name2': name2};
-    print(form);
-    var send = await http.post(Uri.parse(globals.serverURL),
+    Map form = {'treename': treename};
+    var send = await http.post(Uri.parse("http://10.0.2.2:5000/plantTree/"),
         headers: {'Content-Type': 'application/json'}, body: json.encode(form));
 
     print(send.body);
@@ -81,5 +83,62 @@ Future<String> sendToScript(nachricht, name1, name2) async {
     return txId;
   } on Exception catch (e) {
     return e.toString() + "oops";
+  }
+}
+
+Future<String> sendToScript(nachricht, name1, name2, baum) async {
+  await Future.delayed(Duration(seconds: 2));
+  try {
+    Map form = {
+      'nachricht': nachricht,
+      'name1': name1,
+      'name2': name2,
+      'tree': baum
+    };
+    print(form);
+    var send = await http.post(Uri.parse('http://10.0.2.2:5000/'),
+        headers: {'Content-Type': 'application/json'}, body: json.encode(form));
+
+    print(send.body);
+
+    final txId = (json.decode(send.body)).substring(8, 74);
+
+    return txId;
+  } on Exception catch (e) {
+    return e.toString() + "oops";
+  }
+}
+
+Future<String> getLatestTree() async {
+  var bestBlock =
+      await http.get(Uri.parse('http://3.71.71.72:8669/blocks/best'));
+  var bestBlockList = json.decode(bestBlock.body);
+  int best = (bestBlockList['number']);
+  try {
+    Map form = {
+      "range": {"unit": "block", "from": 0, "to": best},
+      "options": {"offset": 0, "limit": 300},
+      "criteriaSet": [
+        {
+          "address": "0xca4B53CF539e30d61D7111cf784BFFA3587C4FE0",
+          "topic0":
+              "0xeb3a151fbf02ed5c90d14b23ba486256b168f6ab2364c5b47319046b11547836"
+        }
+      ],
+      "order": "asc"
+    };
+    var sendToNode = await http.post(
+        Uri.parse('http://3.71.71.72:8669/logs/event'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(form));
+
+    List<dynamic> nodeResponse = json.decode(sendToNode.body);
+    String treeAddress = nodeResponse.last['topics'][2];
+
+    treeAddress = "0x" + treeAddress.substring(26, treeAddress.length);
+
+    return treeAddress;
+  } on Exception catch (e) {
+    return "fail";
   }
 }
