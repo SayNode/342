7e04d1d09feb316e3ba7d791d922f78503685682
Werner Liechti <werner@saynode.ch>
2022-02-09 23:58:37 +0100

    Update to MainNet

--- three4two/lib/Utils/globals.dart
+++ three4two/lib/Utils/globals.dart
@@ -3,14 +3,11 @@ String name1 = "";
 String name2 = "";
 String recentTx = "";
 String newTreeName = "";
-String treeFactoryAddress = "0xca4B53CF539e30d61D7111cf784BFFA3587C4FE0";
-// "0x8F7Ac1738bb189C5ecec3391Ac67e82bB7FBa6DB";
+String treeFactoryAddress = "0x8F7Ac1738bb189C5ecec3391Ac67e82bB7FBa6DB";
 String serverURL =
-    "http://tree2-env.eba-fi58hgdk.eu-central-1.elasticbeanstalk.com/";
-
-//"http://mainnettree-env-1.eba-7ivna8vc.eu-central-1.elasticbeanstalk.com/";
+    "http://mainnettree-env-1.eba-7ivna8vc.eu-central-1.elasticbeanstalk.com/";
 var txId = 1;
-String nodeURL = "http://3.71.71.72:8669/"; //"http://3.124.193.149:8669/";
+String nodeURL = "http://3.124.193.149:8669/";
 List<String> trees = [];
 String currentTree = "";
 
